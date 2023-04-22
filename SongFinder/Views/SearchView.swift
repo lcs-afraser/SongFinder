76c326cef1378d06e5737157620624b8f761eb93
Alistair Fraser <afraser@lcs.on.ca>
2023-04-22 14:37:32 -0400

    Nearly finished adding a search bar; interface is there but search not yet functional.

--- SongFinder/Views/SearchView.swift
+++ SongFinder/Views/SearchView.swift
@@ -14,25 +14,30 @@ struct SearchView: View {
     //Holds the list of songs returned by our search
     @State var foundSongs: [Song] = []
     
+    //Holds the song name the user is searching on
+    @State var searchText = ""
+    
     //MARK: Computed properties
     var body: some View {
-        //Show a list of songs that match the search term
-        List(foundSongs, id: \.trackId) { currentSong in
-            
-            VStack(alignment: .leading) {
-                HStack {
-                    Text(currentSong.trackName)
-                        .bold()
-                    Spacer()
+        NavigationView {
+            List(foundSongs, id: \.trackId) { currentSong in
+                
+                VStack(alignment: .leading) {
+                    HStack {
+                        Text(currentSong.trackName)
+                            .bold()
+                        Spacer()
+                    }
+                    Text(currentSong.collectionName)
+                        .italic()
+                    Text(currentSong.artistName)
                 }
-                Text(currentSong.collectionName)
-                    .italic()
-                Text(currentSong.artistName)
             }
-        }
-        .task {
-            //When the view apepars, fetch search results for a song of our choice
-            foundSongs = await NetworkService.fetch(resultsFor: "as it was")
+            .searchable(text: $searchText)
+            .task {
+                //When the view apepars, fetch search results for a song of our choice
+                foundSongs = await NetworkService.fetch(resultsFor: "as it was")
+            }
         }
     }
 }
