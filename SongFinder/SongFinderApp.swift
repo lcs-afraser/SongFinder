b118eda7bdaec74ddffa6c66d2dbe284a654cfc6
Alistair Fraser <afraser@lcs.on.ca>
2023-04-22 14:10:50 -0400

    Initial Commit

---
+++ SongFinder/SongFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  SongFinderApp.swift
+//  SongFinder
+//
+//  Created by Alistair Fraser on 2023-04-22.
+//
+
+import SwiftUI
+
+@main
+struct SongFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
